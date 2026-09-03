bd6318b845a1fbcb25d7f2693f64371061257a69
Akshaya-Gampala <akshayagampala11@gmail.com>
2026-09-03 16:23:59 +0530

    Update Smart Ambulance Application

--- Smart_Ambulance_Booking_Emergency_Response_System/server/index.js
+++ Smart_Ambulance_Booking_Emergency_Response_System/server/index.js
@@ -39,6 +39,6 @@ app.get('/api/health', (req, res) => {
 handleSockets(io);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,'0.0.0.0' () => {
+app.listen(PORT,'0.0.0.0',() => {
   console.log(`Server running on port ${PORT}`);
 });
